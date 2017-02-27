a23f8bbef359c53ccce9b79e46fb1d8aa25e5e34
Tobias Schubert <contact@tobias-schubert.com>
2017-02-27 21:25:46 +0100

    fix specs (WIP)

--- spec/atom-ternjs-spec.js
+++ spec/atom-ternjs-spec.js
@@ -6,19 +6,20 @@ let path = require('path');
 function sharedSetup() {
 
   atom.project.setPaths([path.join(__dirname, 'fixtures')]);
-  workspaceElement = atom.views.getView(atom.workspace)
+  workspaceElement = atom.views.getView(atom.workspace);
 
   waitsForPromise(() => {
 
-    return atom.packages.activatePackage('atom-ternjs').then((pkg) => {
+    return new Promise((resolve, reject) => {
 
-      pack = pkg.mainModule;
+      atom.workspace.open('test.js')
+      .then(() => {
+
+        pack = atom.packages.enablePackage('atom-ternjs');
+
+        resolve();
+      });
     });
-  });
-
-  waitsForPromise(() => {
-
-    return atom.workspace.open('test.js');
   });
 
   runs(() => {
@@ -37,14 +38,9 @@ describe('atom-ternjs', () => {
 
   describe('activate()', () => {
 
-    it('activates atom-ternjs and initializes the autocomplete-plus provider', () => {
+    it('activates the atom-ternjs-manager', () => {
 
-      expect(pack.provider).toBeDefined();
-    });
-
-    it('activates atom-ternjs and initializes the manager', () => {
-
-      expect(pack.manager).toBeDefined();
+      expect(pack.config).toBeDefined();
     });
   });
 
@@ -56,20 +52,14 @@ describe('atom-ternjs', () => {
       atom.packages.deactivatePackage('atom-ternjs');
     });
 
-    it('deactivates atom-ternjs', () => {
-
-      expect(pack.manager).toBeUndefined();
-      expect(pack.provider).toBeUndefined();
-    });
-
     it('destroys all views', () => {
 
       expect(workspaceElement.querySelectorAll('atom-ternjs-reference').length).toBe(0);
       expect(workspaceElement.querySelectorAll('atom-ternjs-rename').length).toBe(0);
-      expect(workspaceElement.querySelectorAll('atom-ternjs-config').length).toBe(0);
+      expect(workspaceElement.querySelectorAll('.atom-ternjs-config').length).toBe(0);
       expect(workspaceElement.querySelectorAll('atom-ternjs-documentation').length).toBe(0);
       expect(workspaceElement.querySelectorAll('atom-ternjs-type').length).toBe(0);
-      expect(editorElement.querySelectorAll('atom-text-editor .scroll-view .atom-ternjs-definition-marker').length).toBe(0);
+      expect(editorElement.querySelectorAll('atom-text-editor .atom-ternjs-definition-marker').length).toBe(0);
     });
   });
 });
